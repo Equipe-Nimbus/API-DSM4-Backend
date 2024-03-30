fefe9d621f3bafae99b1f5bb5e6ba9749a5d9f82
Bruno Denardo <brunoden.bd@gmail.com>
2024-03-30 13:17:15 -0300

    test: Testando merge quando um teste falha

--- test/teste.test.ts
+++ test/teste.test.ts
@@ -5,6 +5,6 @@ test('Este teste sempre passa', () => {
     expect(true).toBe(true);
   });
 test('Este teste nunca passa passa', () => {
-    expect(true).toBe(true);
+    expect(true).toBe(false);
   });
  
