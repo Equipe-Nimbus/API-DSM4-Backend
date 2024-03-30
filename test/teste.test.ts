1fb70083b0a271cba328067acf0482eda920b668
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-03-30 13:23:16 -0300

    Testando merge quando um teste falha (#9)
    * test: teste de merge
    
    * Adicionando o auto-aprove no TesteUnitarioCI.yml
    
    * fix: concertando a condicional da atividade 'Aprovando se os testes passarem'
    
    * fix: Diminuindo versões de teste para node para 1
    
    * fix: trocando token de aceeso para a aprovacao de pull request
    
    * test: Inserindo teste que nunca passa
    
    * feat: auto-merge
    
    * Testando resgate de nome de repositorios no workflow
    
    * fix: Ajustando o campo repo no TesteUnitarioCI.yml
    
    * fix: tirando o campo repo
    
    * test: Testando merge quando um teste falha
    
    * test: comentando o teste que sempre falha

--- test/teste.test.ts
+++ test/teste.test.ts
@@ -4,7 +4,9 @@
 test('Este teste sempre passa', () => {
     expect(true).toBe(true);
   });
-test('Este teste nunca passa passa', () => {
-    expect(true).toBe(true);
+/*test('Este teste nunca passa passa', () => {
+    expect(true).toBe(false);
   });
+*/
+
  
