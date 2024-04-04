23f86c01ed12e9dffc675786e158568a1652abc2
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-04-04 20:08:54 -0300

    Feature: atualizacao do nome do tipo parametro (#34)
    * fix: Deixando o perfil do usuario nullable
    
    * Feat: Listagem paginada de Tipo Parametro
    
    * Feat: Alteracao do nome do tipoParametro

--- src/routes/routesTipoParametro.ts
+++ src/routes/routesTipoParametro.ts
@@ -6,5 +6,7 @@ const routesTipoParametro = Router();
 routesTipoParametro.post("/cadastrar", TipoParametroController.cadastrar)
 routesTipoParametro.delete("/deletar/:id", TipoParametroController.deletar)
 routesTipoParametro.get("/listarGeral/paginada", TipoParametroController.listarPaginada)
+routesTipoParametro.put("/atualizar", TipoParametroController.atualizar)
+
 
 export default routesTipoParametro;
