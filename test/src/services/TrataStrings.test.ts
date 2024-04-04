847ef45eaf6892f2224a6c2fb1083d3bca5ca280
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-04-04 08:16:10 -0300

    Fix: Trocando tratamento de string de lower para upper case (#30)
    * fix: Deixando o perfil do usuario nullable
    
    * Feat: Alteracao Usuario
    
    * Fix: Adicionando a possibilidade de alterar o perfil do usuario
    
    * Feat: Cadastro tipo parametro sem teste unitario
    
    * Test: Testes unitario cadastro do TipoParametroController
    
    * Fix: Corrigindo erros de duplicacao da funcao atualizar
    
    * Feat: tratamento de strings cadastradas do tipoParametro
    
    * Fix: Tirando a conferencia de unidades repetidas
    
    * Fix: Trocando tratamento de stings de lower para upper case

--- test/src/services/TrataStrings.test.ts
+++ test/src/services/TrataStrings.test.ts
@@ -5,9 +5,9 @@ import TrataStrings from "../../../src/services/TrataStrings"
 describe("Teste da classe que trata strings, tira acesntos e transforma em lower case", ()=>{
 
     test("Tira acento e deixa em caixa baixa", ()=>{
-        var stringSemTratamento = "CÉU"
+        var stringSemTratamento = "cÉU"
         const stringComTratamento = TrataStrings.tratarParaLowerSemAcento(stringSemTratamento)
-        expect(stringComTratamento).toBe("ceu")
+        expect(stringComTratamento).toBe("CEU")
     })
 
 })
