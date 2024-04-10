8f6b3c58278e5c99aa04baa0e6c5eaaec648eede
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-04-10 16:08:33 -0300

    Feature: listagem paginada de alertas (#48)
    * Feature: Listagem paginada de alertas sem tratamentos de erros e testes
    
    * Feature: listagem paginada com tratamento de erro e teste unitario

--- src/routes/routesAlerta.ts
+++ src/routes/routesAlerta.ts
@@ -5,5 +5,7 @@ const routesAlerta = Router();
 
 routesAlerta.post("/cadastrar", AlertaController.cadastrar);
 routesAlerta.delete("/deletar/:idAlerta", AlertaController.deletar);
+routesAlerta.get("/listarGeral/paginada", AlertaController.listarPaginada);
+
 
 export default routesAlerta;
---
+++ src/services/Alerta/TrataValoresFiltroAlerta.ts
@@ -0,0 +1,34 @@
+import { Request, query } from "express";
+import { FindManyOptions, Like } from "typeorm";
+import { Alerta } from "../../entities/Alerta";
+import InterfaceFiltroSelecao from "../InterfaceFiltroSelecao";
+import TrataValoresFiltro from "../TrataValoresFiltro";
+
+
+class TrataValoresFiltroAlerta extends TrataValoresFiltro<Alerta> {
+    
+    
+    tratarContagem(req: Request): FindManyOptions<Alerta> {
+        const nomeAlerta = req.query.nome ?
+            "%" + req.query.nome + "%" : "%%"
+        
+        const filtro:FindManyOptions<Alerta> ={where:{nomeAlerta: Like(`${nomeAlerta}`)}}
+        return filtro
+    
+    }
+    
+    tratarSelect(req: Request): InterfaceFiltroSelecao {
+        const nomeAlerta = req.query.nome ?
+            "%" + req.query.nome + "%" : "%%"
+
+        const filtro ={
+            query:`alerta.nomeAlerta LIKE :nome`,
+            valores:{nome:nomeAlerta}
+        }
+
+        return filtro
+    }
+    
+}
+
+export default new TrataValoresFiltroAlerta()
