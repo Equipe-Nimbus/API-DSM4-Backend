98d1f9d93ae84f2d76f5158e1ef7d7efc63e3853
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-04-10 16:26:58 -0300

    Feature: rotas com verificacao token (#49)
    * fix: Deixando o perfil do usuario nullable
    
    * Feat: Listagem paginada de Tipo Parametro
    
    * Feat: Listagem Especifica de Tipo Parametro
    
    * Feat: Listagem de Tipo Parametro para selecao (Sera usada no cadastro de Estacao)
    
    * Fix: Listagem de TipoParametro para selecao só retornam os de status true
    
    * Feat: Login e geracao de token
    
    * Feat: rotas com verificacao de token
    
    * Feat: Adicionando o perfil de usuário à geracao de token

--- src/routes/routesAlerta.ts
+++ src/routes/routesAlerta.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import AlertaController from "../controllers/AlertaController";
+import VerificadorToken from "../middlewares/VerificadorToken";
 
 const routesAlerta = Router();
 
-routesAlerta.post("/cadastrar", AlertaController.cadastrar);
-routesAlerta.delete("/deletar/:idAlerta", AlertaController.deletar);
-routesAlerta.get("/listarGeral/paginada", AlertaController.listarPaginada);
+routesAlerta.post("/cadastrar", VerificadorToken.verificar, AlertaController.cadastrar);
+routesAlerta.delete("/deletar/:idAlerta", VerificadorToken.verificar, AlertaController.deletar);
+routesAlerta.get("/listarGeral/paginada", VerificadorToken.verificar, AlertaController.listarPaginada);
 
 
 export default routesAlerta;
