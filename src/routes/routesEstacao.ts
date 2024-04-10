d1c5d5cbc587590d872684e8a19934a102ebadf7
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-04-10 20:57:16 -0300

    Feature: listagem para selecao estacao (#52)
    * Feature: Delecao logica de estacao
    
    * Feature: Listagem para selecao de Estacao e alteracao na listagem para selecao de tipoParametro

--- src/routes/routesEstacao.ts
+++ src/routes/routesEstacao.ts
@@ -7,6 +7,7 @@ const routesEstacao = Router();
 routesEstacao.post("/cadastrar", VerificadorToken.verificar, EstacaoController.cadastrar);
 routesEstacao.get("/listarGeral/paginada", VerificadorToken.verificar, EstacaoController.listarPaginada);
 routesEstacao.get("/listarEspecifico/:id", VerificadorToken.verificar, EstacaoController.listarEspecifico);
+routesEstacao.get("/listarParaSelecao", VerificadorToken.verificar, EstacaoController.listarParaSelecao);
 routesEstacao.delete("/deletar/:idEstacao", VerificadorToken.verificar, EstacaoController.deletar);
 
 
