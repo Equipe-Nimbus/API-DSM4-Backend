259483f7a8c1f12849112e101ebdbdaf3e04db45
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-05-04 23:34:55 -0300

    Fix: aplicacao offset (#92)
    * Fix: Corrigindo conferencia de igualdade de alertas no cadastro
    
    * Fix: filtro de listagem de estacao agora usa cidade
    
    * Fix: Corrigindo as alteracoes de tipoParametro
    
    * Fix: Corrigindo cadastro de estacao devido a troca para uuid
    
    * Refac: refatorando a atualizacao de estacoes ativas por mes
    
    * Fix:Concertando aplicacao de offset

--- src/services/Dashboard/FormataMedicoesDeHoje.ts
+++ src/services/Dashboard/FormataMedicoesDeHoje.ts
@@ -12,17 +12,17 @@ class FormataMedicoesDeHoje{
             let parametroDashboard = this.criarParametroDashboard()
             parametroDashboard.unidadeMedida = (await parametros[parametro].tiposParametro).unidadeTipoParametro
             parametroDashboard.nomeTipoParametro = (await parametros[parametro].tiposParametro).nomeTipoParametro
-            let offset = (await parametros[parametro].tiposParametro).offsetTipoParametro
+            let offset = Number((await parametros[parametro].tiposParametro).offsetTipoParametro)
             let fator = (await parametros[parametro].tiposParametro).fatorTipoParametro
-            parametroDashboard.valorMaximo = (parametros[parametro].medicoes[0].valorMedida * fator) - offset
-            parametroDashboard.valorMinimo = (parametros[parametro].medicoes[0].valorMedida * fator) - offset
+            parametroDashboard.valorMaximo = (parametros[parametro].medicoes[0].valorMedida * fator) + offset
+            parametroDashboard.valorMinimo = (parametros[parametro].medicoes[0].valorMedida * fator) + offset
             parametros[parametro].medicoes.forEach(medicao =>{
-                if(parametroDashboard.valorMaximo < (medicao.valorMedida * fator) - offset)
-                    parametroDashboard.valorMaximo = (medicao.valorMedida * fator) - offset
-                if(parametroDashboard.valorMinimo > (medicao.valorMedida * fator) - offset)
-                    parametroDashboard.valorMinimo = (medicao.valorMedida * fator) - offset
+                if(parametroDashboard.valorMaximo < (medicao.valorMedida * fator) + offset)
+                    parametroDashboard.valorMaximo = (medicao.valorMedida * fator) + offset
+                if(parametroDashboard.valorMinimo > (medicao.valorMedida * fator) + offset)
+                    parametroDashboard.valorMinimo = (medicao.valorMedida * fator) + offset
                 parametroDashboard.medicoes.push({
-                    valor: (medicao.valorMedida * fator) - offset,
+                    valor: (medicao.valorMedida * fator) + offset,
                     data: new Date((medicao.unixTime * 1000) - (3 * 60 * 60 * 1000))
                 })
             })
