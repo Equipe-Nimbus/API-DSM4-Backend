04360739459d6599b2cd1a7ae3b5befd2a055690
PedroHSdeAlmeida <106109294+PedroHSdeAlmeida@users.noreply.github.com>
2024-05-26 02:14:45 -0300

    feat(RelatorioQuantidadeAlerta): Classe com a logica para gerar o relatorio de ocorrencias filtrando a  data e a localização

---
+++ src/services/Relatorio/GeraRelatorioQuantidadeAlerta.ts
@@ -0,0 +1,70 @@
+import FiltraLocalizacao from "./FiltraLocalizacao";
+import FiltraData from "./FiltraData";
+
+class GeraRelatorioQuantidadeAlerta {
+  async geraRelatorio(cidade: string, estado: string, dataInicio: string, dataFim: string) {
+    const localizacao = await FiltraLocalizacao.filtra(cidade, estado);
+
+    let estacoesNomes = [];
+
+    if (cidade) {
+      estacoesNomes = localizacao.estacoes;
+    } else if (estado) {
+      estacoesNomes = localizacao.cidades.flatMap(cidade => cidade.estacoes);
+    }
+
+    const medicoes = await FiltraData.filtra(estacoesNomes, dataInicio, dataFim);
+
+    const formatarMedicoes = (medicoes, estacaoNome) => {
+      return medicoes
+        .filter(medicao => medicao.nomeEstacao === estacaoNome)
+        .map(medicao => ({
+          nomeAlerta: medicao.nomeAlerta,
+          medicao: medicao.medicao,
+          data: medicao.data,
+          nomeTipoParametro: medicao.nomeTipoParametro
+        }));
+    };
+
+    if (cidade) {
+      const estacoesComMedicoes = estacoesNomes.map(estacaoNome => {
+        const medicoesEstacao = medicoes.filter(medicao => medicao.nomeEstacao === estacaoNome);
+        return {
+          nome: estacaoNome,
+          medicoes: formatarMedicoes(medicoesEstacao, estacaoNome)
+        };
+      });
+
+      return {
+        nome: cidade,
+        estacoes: estacoesComMedicoes
+      };
+    }
+
+    if (estado) {
+      const cidadesComMedicoes = localizacao.cidades.map(cidadeObj => {
+        const estacoesComMedicoes = cidadeObj.estacoes.map(estacaoNome => {
+          const medicoesEstacao = medicoes.filter(medicao => medicao.nomeEstacao === estacaoNome);
+          return {
+            nome: estacaoNome,
+            medicoes: formatarMedicoes(medicoesEstacao, estacaoNome)
+          };
+        });
+
+        return {
+          nome: cidadeObj.nome,
+          estacoes: estacoesComMedicoes
+        };
+      });
+
+      return {
+        estado: estado,
+        cidades: cidadesComMedicoes
+      };
+    }
+
+    throw new Error('Erro ao processar a solicitação');
+  }
+}
+
+export default new GeraRelatorioQuantidadeAlerta();
