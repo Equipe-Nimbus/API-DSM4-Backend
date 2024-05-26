ca8f33af4c8e759007860835d9ffa8b654f69e11
PedroHSdeAlmeida <106109294+PedroHSdeAlmeida@users.noreply.github.com>
2024-05-26 02:16:25 -0300

    feat(RelatorioQuantidadeAlerta): Criando controller do relatorio de quantidade de alertas, com a logica para receber os parametros e retornar o relatorio

--- src/controllers/RelatorioController.ts
+++ src/controllers/RelatorioController.ts
@@ -1,9 +1,16 @@
+import GeraRelatorioQuantidadeAlerta from "../services/Relatorio/GeraRelatorioQuantidadeAlerta";
+import { Request, Response } from "express";
+
 
 
 class RelatorioController {
-    geraRelatorioQuantidadeAlerta() {
-        
+    async geraRelatorioQuantidadeAlerta(req: Request, res: Response) {
+        const { cidade, estado, dataInicio, dataFim } = req.body;
 
+        const resultado = await GeraRelatorioQuantidadeAlerta.geraRelatorio(cidade, estado, dataInicio, dataFim);
+
+        res.status(200).send(resultado);
+        console.log(resultado);
     };
 };
 
