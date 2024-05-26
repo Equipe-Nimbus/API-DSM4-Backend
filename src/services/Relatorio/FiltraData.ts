ba52c4f91610ffedb53cafdf0227cef935ebd72d
PedroHSdeAlmeida <106109294+PedroHSdeAlmeida@users.noreply.github.com>
2024-05-26 14:53:50 -0300

    bugfix(RelatorioQuantidadeAlerta): A função não estava resgatando as medições referentes ao mesmo dia da "dataFim" recebida, agora está

--- src/services/Relatorio/FiltraData.ts
+++ src/services/Relatorio/FiltraData.ts
@@ -4,25 +4,28 @@ class FiltraData {
   async filtra(estacoesNomes: string[], dataInicio: string, dataFim: string) {
     await MongoDB.connect();
     const colecaoEstacoes = MongoDB.db("BackNimbusNaoRelacional").collection("OcorrenciaAlertas");
-
+  
     if (estacoesNomes.length === 0) {
       throw new Error('Não há estações para filtrar os dados.');
     }
-
-    const dataInicioUnix = new Date(dataInicio).getTime() / 1000;
-    const dataFimUnix = new Date(dataFim).getTime() / 1000;
-
+  
+    const dataInicioDate = new Date(`${dataInicio}T00:00:00Z`);
+    const dataInicioUnix = dataInicioDate.getTime() / 1000;
+  
+    const dataFimDate = new Date(`${dataFim}T23:59:59.999Z`);
+    const dataFimUnix = dataFimDate.getTime() / 1000;
+  
     const alertas = await colecaoEstacoes.find({
       nomeEstacao: { $in: estacoesNomes },
       unixtime: { $gte: dataInicioUnix, $lte: dataFimUnix }
     }).toArray();
-
+  
     await MongoDB.close();
-
+  
     if (alertas.length === 0) {
       throw new Error('Não há medições disponíveis para o período especificado.');
     }
-
+  
     return alertas.map(alerta => ({
       nomeAlerta: alerta.nomeAlerta,
       medicao: alerta.medicao,
@@ -33,7 +36,6 @@ class FiltraData {
       estadoAlerta: alerta.estadoEstacao,
       unidadeMedicao: alerta.unidadeMedicao
     }));
-  }
-}
+  }}
 
 export default new FiltraData();
