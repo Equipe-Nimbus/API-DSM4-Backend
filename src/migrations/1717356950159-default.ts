c2785a38f92cfd70404875312ba94820aacd328a
Bruno Denardo <112097038+brunodenardo@users.noreply.github.com>
2024-06-02 21:09:37 -0300

    Feat: relatorio max min (#103)
    * Feat(relatorio): Adicionando o atributo tagTemporal a entidade Medicao
    
    Esse atributo tera uma string que contem o mes e ano da medicao, que vao
    ser necessarios na hora de agrupar as medicoes minimas e maximas
    
    EN-303
    
    * Feat(relatorio): Relatorio de medicoes max e min
    
    EN-303

---
+++ src/migrations/1717356950159-default.ts
@@ -0,0 +1,16 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class Default1717356950159 implements MigrationInterface {
+    name = 'Default1717356950159'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "medicao" ADD "tagTemporal" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "estacao" ALTER COLUMN "idPlacaEstacao" SET NOT NULL`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "estacao" ALTER COLUMN "idPlacaEstacao" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "medicao" DROP COLUMN "tagTemporal"`);
+    }
+
+}
--- src/routes/routesRelatorios.ts
+++ src/routes/routesRelatorios.ts
@@ -6,5 +6,6 @@ const routesRelatorio = Router();
 
 routesRelatorio.post("/quantidadeAlerta", RelatorioController.geraRelatorioQuantidadeAlerta);
 routesRelatorio.get("/medicoes/:inicio/:fim/:idEstacao", RelatorioController.geraRelatorioMedicao);
+routesRelatorio.get("/minmax/:inicio/:fim/:idEstacao", RelatorioController.geraRelatorioMinMax);
 
 export default routesRelatorio;
